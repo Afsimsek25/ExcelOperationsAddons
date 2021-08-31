aaff44350656052a96d6038788fede239f2651b6
Afsimsek25 <asimsekfurkan@yandex.com>
2021-08-31 23:12:09 +0300

    Update Cell

---
+++ src/main/Addon/getSumOfTheColumnValues.java
@@ -0,0 +1,13 @@
+package main.Addon;
+
+import io.testproject.java.sdk.v2.addons.WebAction;
+import io.testproject.java.sdk.v2.addons.helpers.WebAddonHelper;
+import io.testproject.java.sdk.v2.enums.ExecutionResult;
+import io.testproject.java.sdk.v2.exceptions.FailureException;
+
+public class getSumOfTheColumnValues implements WebAction {
+    @Override
+    public ExecutionResult execute(WebAddonHelper helper) throws FailureException {
+        return null;
+    }
+}
